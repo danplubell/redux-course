931b61a77ac5e6192b81328db21722d4e61c9481
Dan Plubell <dplubell@cerner.com>
2017-08-16 13:00:45 -0500

    refactoring and simplify with react-redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import {updateCurrent} from './reducers/todo';
+
+
 
 class App extends Component {
   render() {
@@ -14,7 +18,7 @@ class App extends Component {
         </div>
         <div className="Todo-App"></div>
           <TodoForm currentTodo={this.props.currentTodo}
-            changeCurrent={this.props.changeCurrent}
+            changeCurrent={this.props.updateCurrent}
           />
           <TodoList todos={this.props.todos}/>
       </div>
@@ -22,4 +26,20 @@ class App extends Component {
   }
 }
 
-export default App;
+//const actions = bindActionCreators({updateCurrent}, store.dispatch); //using shorthand notation for name value pairs
+
+//export default App;
+//create connected app
+//const mapStateToProps = (state) => state; //represents the subset of state to pass to component, in this case we want it all
+//provider and connect function work together to get props to component
+
+//const mapDispatchToProps = {updateCurrent}; //updateCurrent is the action creator from the reducer todo.js
+
+//const ConnectedApp = connect(mapStateToProps,mapDispatchToProps)(App);
+//export default ConnectedApp;
+
+//shorten all that down to...
+export default connect(
+    (state)=> state,
+    {updateCurrent}
+    )(App);
--- src/index.js
+++ src/index.js
@@ -1,34 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 import store from './store';
-import {bindActionCreators} from 'redux';
-import {updateCurrent} from './reducers/todo';
 import registerServiceWorker from './registerServiceWorker';
 
-//const todoChangeHandler = (val) => store.dispatch(updateCurrent(val))
 
-//bind actions to name value pairs of functions and action creators
-//name value pairs of function and action creators and reference to dispatch function
 
-//const actions = bindActionCreators({
-//    todoChangeHandler: updateCurrent}, store.dispatch);
-//this can be changed to:
+ReactDOM.render(
+    //provider takes care of getting the state and subscribing to state changes
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
-const actions = bindActionCreators({updateCurrent}, store.dispatch);
-
-const render = () => {
-    const state = store.getState();
-    ReactDOM.render(<App todos={state.todos}
-                         currentTodo={state.currentTodo}
-                         changeCurrent={actions.updateCurrent} //get change handler from bound actions
-                    />,
-        document.getElementById('root'));
-
-}
-render();
-store.subscribe(render);
 
 
 registerServiceWorker();
