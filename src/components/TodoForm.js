6db904999b878c087c8810510ab2a6c90019cc20
Dan Plubell <dplubell@cerner.com>
2017-08-16 10:08:30 -0500

    initial version that uses only redux

---
+++ src/components/TodoForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default (props) => {
+    /*destructure props*/
+    const {currentTodo,changeCurrent} = props;
+    const handleInputChange = (event) => {
+        const val = event.target.value;
+        changeCurrent(val);
+    }
+    return (
+            <form>
+                <input type="text"
+                       value={currentTodo}
+                        onChange={handleInputChange}
+                />
+            </form>
+
+    )
+
+}
